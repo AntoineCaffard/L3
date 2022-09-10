2813f25f5975ba026695b7cad3766352b60b20e1
AntoineCaffard <110288156+AntoineCaffard@users.noreply.github.com>
2022-09-10 15:34:41 +0200

    Create TD1-8.c

---
+++ C Avancé/TD/TD1/TD1-8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    scanf("%d%d", &num1, &num2);
+    printf("num1 = %d\n", num1);
+    printf("num2 = %d\n", num2);
+    num1 = num1 ^ num2;
+    num2 = num1 ^ num2;
+    num1 = num1 ^ num2;
+    printf("num1 aprés le swap = %d\n", num1);
+    printf("num2 aprés le swap = %d\n", num2);
+    return 0;
+}
